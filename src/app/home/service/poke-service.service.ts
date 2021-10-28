48819f4f8a000e18f2e18380f27eb58f4dc4ba6d
PedCoelho <cabralcoelho@gmail.com>
2021-10-28 16:00:23 -0300

    [REFACTORED] USING ACTUAL TYPES

--- src/app/home/service/poke-service.service.ts
+++ src/app/home/service/poke-service.service.ts
@@ -1,37 +1,37 @@
+import { PokeApiPageData } from './../models/pokeapilistentry.interface';
 import { PokemonDetail } from './../models/pokemon-detail.interface';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { PokeApiListEntry } from '../models/pokeapilistentry.interface';
 
-
-let pokimane: PokemonDetail[] = []
-let API_URL: string = 'https://pokeapi.co/api/v2/pokemon/?offset='
+const API_URL: string = 'https://pokeapi.co/api/v2/pokemon/?offset='
 
 @Injectable()
-
 export class PokeService {
+
+  private pokimane: PokemonDetail[] = []
 
   constructor(private http: HttpClient) { }
 
   getPage(page: number) {
-    return this.http.get<PokeApiListEntry[]>(API_URL + page)
-    //maybe should be handled internally
+    return this.http.get<PokeApiPageData>(API_URL + page)
   }
 
   getDetails(url: string) {
     // if (url) {
     //todo como fazer para inserir uma condição aqui? 
-    //todo o que fazer aqui caso o http request falhe 
-    return this.http.get(url)
+    //review o componente consumidor espera um Observable, como fazer ele ficar de boa com um retorno vazio ou nenhum retorno 
+
+    return this.http.get<PokemonDetail>(url)
     // }
+    //todo o que fazer aqui caso o http request falhe 
   }
 
   get() {
-    return pokimane.slice()
+    return this.pokimane.slice()
   }
 
   add(poki: PokemonDetail) {
-    pokimane.push(poki)
+    this.pokimane.push(poki)
     return this.get()
   }
 }
