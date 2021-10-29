306964e4a073ff4ba9c41a83bcfef1854aecc2f3
PedCoelho <cabralcoelho@gmail.com>
2021-10-29 18:31:20 -0300

    [ADDED] Redirect

--- src/app/home/components/pokimane/pokimane.component.ts
+++ src/app/home/components/pokimane/pokimane.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { PokemonDetail } from '../../models/pokemon-detail.interface';
 
 @Component({
@@ -13,7 +14,15 @@ export class PokimaneComponent implements OnInit {
 
   types: string[] = []
 
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router) { }
+
   ngOnInit() {
     this.types = this.pokimane.types.map(({ type }) => type.name)
+  }
+
+  public gotoDetail(): void {
+    this.router.navigate(['/detail', { id: this.pokimane.id }]);
   }
 }
