a1287d75798ed8894ac9a8a150d2488c407123aa
pedro.silva <pedro.silva@mjv.com.br>
2021-11-01 23:00:29 -0300

    [SMALL REFACTOR] [ADDED NG-CONTENT PLACEHOLDER]

--- src/app/home/components/poke-grid/poke-grid.component.ts
+++ src/app/home/components/poke-grid/poke-grid.component.ts
@@ -1,6 +1,6 @@
-import { PokemonDetail } from './../../models/pokemon-detail.interface';
+import { PokeApiPageData } from './../../models/pokeapilistentry.interface';
 import { Component, OnInit } from '@angular/core';
-import { concat, from, of } from 'rxjs';
+import { concat } from 'rxjs';
 
 import { PokeService } from '../../service/poke-service.service'
 
@@ -12,54 +12,41 @@ import { PokeService } from '../../service/poke-service.service'
 
 export class PokeGridComponent implements OnInit {
 
-  public poki: PokemonDetail[] = []
   public next_page?: number
 
   constructor(private pokeService: PokeService) { }
 
   /* -------------------------------------------------------------------------- */
-  /*                          option 1 : non sequential                         */
-  /* -------------------------------------------------------------------------- */
-  // getResults(page: number) {
-  //   this.pokeService.getPage(page).subscribe((res: any) => {
-
-  //     console.log(res)
-
-  //     res?.results.forEach((pokimane: any) => {
-  //       this.pokeService.getDetails(pokimane.url)?.subscribe((poki: any) => {
-  //         this.poki = this.pokeService.add(poki)
-  //       })
-  //     })
-  //   })
-  // }
-
-  /* -------------------------------------------------------------------------- */
   /*         option 2 : build observable array and request sequentially         */
   /* -------------------------------------------------------------------------- */
-  public getResults(page: number) {
+  private getResults(page: number): void {
     this.pokeService.getPage(page).subscribe((page_data) => {
-      console.log(page_data)
 
-      // let page = [...new URLSearchParams(page_data.next).values()][0]
-      //ESSA PORRA FUNCIONA NO BROWSER, MAS NÃO FUNCIONA AQUI PORQUE C****** ?
-
-      let reg = new RegExp(/\?offset=(.*?)&/)
-      let pageN: string | null = (page_data.next?.match(reg) || [null, null])[1] //gambiarra anti-typescript
-
-      this.next_page = Number(pageN)
-      console.log(pageN, this.next_page)
+      this.setNextPage(page_data)
 
       let observables = page_data.results.map((pokimane) => {
         return this.pokeService.getDetails(pokimane.url)
       })
 
       concat(...observables).subscribe((pokimane_detail) => {
-        this.poki = this.pokeService.add(pokimane_detail)
+        this.pokeService.add(pokimane_detail)
       })
     })
   }
 
-  public getNextPage() {
+  setNextPage(api_response: PokeApiPageData): void {
+    console.log(api_response)
+    // let page = [...new URLSearchParams(api_response.next).values()][0]
+    //ESSA PORRA FUNCIONA NO BROWSER, MAS NÃO FUNCIONA AQUI PORQUE C****** ?
+
+    let reg = new RegExp(/\?offset=(.*?)&/)
+    let pageN: string | null = (api_response.next?.match(reg) || [null, null])[1] //gambiarra anti-typescript
+
+    this.next_page = Number(pageN)
+    console.log(this.next_page)
+  }
+
+  getNextPage(): void {
     let next = this.next_page
     if (next) {
       this.getResults(next)
