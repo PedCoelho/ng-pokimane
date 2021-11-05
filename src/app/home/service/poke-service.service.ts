a3465497ec327a310e5c3ca606a96f50ff06972e
pedro.silva <pedro.silva@mjv.com.br>
2021-11-05 18:09:07 -0300

    [SMALL SERVICE REFACTOR]

--- src/app/home/service/poke-service.service.ts
+++ src/app/home/service/poke-service.service.ts
@@ -1,30 +1,35 @@
 import { PokeApiPageData } from './../models/pokeapilistentry.interface';
-import { PokeDetail, PokemonDetail } from './../models/pokemon-detail.interface';
+import { PokeDetail, PokeApiDetail } from './../models/pokemon-detail.interface';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, Observer } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 
-const API_URL: string = 'https://pokeapi.co/api/v2/pokemon/?offset='
+let API_URL: string = 'https://pokeapi.co/api/v2/pokemon/?offset='
 
 @Injectable()
 export class PokeService {
 
   private pokimane: PokeDetail[] = []
 
+  private next_page?: number
+  private nextPageUrl: string = ''
+
   constructor(private http: HttpClient) { }
 
   getPage(page: number): Observable<PokeApiPageData> {
-    return this.http.get<PokeApiPageData>(API_URL + page + '&limit=151')
+    return this.http.get<PokeApiPageData>(API_URL + page + '&limit=151').pipe(
+      tap(response => this.getNextPageUrl(response))
+    )
   }
 
   getDetails(url: string) {
     //review como fazer para inserir uma condição aqui? 
     //review o componente consumidor espera um Observable, como fazer ele ficar de boa com um retorno vazio ou nenhum retorno 
     // if (url) {
-
     //review tentativa de simplificar a resposta da API para que os componentes tenham menos trabalho
-    return this.http.get<PokemonDetail>(url).pipe(
+
+    return this.http.get<PokeApiDetail>(url).pipe(
       map(({ name, id, types, sprites, ...rest }) => {
         const revised_types: string[] = types.map(x => x.type.name)
         const revised_sprites: { main: string, other: string } = { main: sprites.other.home.front_default, other: sprites.front_default }
@@ -51,12 +56,31 @@ export class PokeService {
     /* -------------------------------------------------------------------------- */
     return this.pokimane.slice()
   }
+
   // get(): Observable<PokeDetail[]> {
   //   /* -------------------------------------------------------------------------- */
   //   /*                                 asynchronous                               */
   //   /* -------------------------------------------------------------------------- */
-  //   return new Observable((observer: Observer<PokeDetail[]>) => { observer.next(this.pokimane.slice()) })
+  //   return of(this.pokimane.slice())
   // }
+
+  private getNextPageUrl(response: PokeApiPageData) {
+    console.log(response.next)
+    if (!response.next) return
+    this.nextPageUrl = response.next
+  }
+
+  public getNextPageN(): number | undefined {
+    return this.next_page
+  }
+
+  public setNextPageN(pageN: number): number | undefined {
+    if (pageN) {
+      this.next_page = pageN
+      return this.next_page
+    }
+    return
+  }
 }
 
 
